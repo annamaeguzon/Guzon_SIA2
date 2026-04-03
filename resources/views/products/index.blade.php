bec04eaa9ebb3f7003f2989b9f36970d3ac977fe
guzonannamae <guzonannamae14@gmail.com>
2026-04-03 18:45:59 +0800

    feat: implement product MVC structure

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,7 @@
+@if($products->isEmpty())
+    <p>No products found. (Try adding some in your database!)</p>
+@else
+    @foreach($products as $product)
+        <p>{{ $product->name }} - {{ number_format($product->price, 2) }} (Qty: {{ $product->quantity }})</p>
+    @endforeach
+@endif
